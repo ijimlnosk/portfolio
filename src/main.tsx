be1d437a0f9e48a3bc0a24c9b3ba457f97b05206
ijimnlosk <skek298@naver.com>
2024-09-20 01:15:44 +0900

    msw 및 react query 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+        },
+    },
+})
+
+async function prepare() {
+    const { worker } = await import("./mocks/browser")
+    return worker.start({
+        onUnhandledRequest: "bypass", // MSW에서 처리하지 않는 요청은 무시
+    })
+}
+
+prepare().then(() => {
+    createRoot(document.getElementById("root")!).render(
+        <StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </StrictMode>,
+    )
+})
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,59 @@
+import { http, HttpResponse } from "msw"
+import { ProjectItem } from "../component/project/type"
+
+const mockProjects: ProjectItem[] = [
+    {
+        id: 1,
+        title: "Yeo-gi",
+        subTitle: "여행 정보 커뮤니티",
+        content: "",
+        thumbnail: "https://github.com/user-attachments/assets/2f054f96-6c6c-4e23-bf73-8224ff556849",
+        period: "2024.05 - 2024.08",
+        skill: ["Next.js", "TypeScript"],
+        library: ["React-Query", "Next-Auth", "tailwindCSS", "Zustand"],
+        link: [
+            { title: "깃허브", type: "github", url: "https://github.com/mobi-projects/yeogi-client" },
+            {
+                title: "회고",
+                type: "vlog",
+                url: "https://velog.io/@skek298/Yeo-gi-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-KPT-%ED%9A%8C%EA%B3%A0",
+            },
+            {
+                title: "기획서",
+                type: "figma",
+                url: "https://www.figma.com/design/VoBkptzgv2kOQYvWmfO0ln/%EC%97%AC%EA%B8%B0-%EA%B8%B0%ED%9A%8D%EC%84%9C?node-id=0-1&t=mZIuUUQ188zbjtG0-1",
+            },
+        ],
+        distributeLink: "https://yeogi-client.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "Purr-Tunes",
+        subTitle: "고양이가 상호작용하는 Youtube Music",
+        content: "",
+        thumbnail: "https://github.com/user-attachments/assets/fccc5cc1-a7d1-4995-8511-559a7b6a5d46",
+        period: "2024.08 - 진행중",
+        skill: ["Next.js", "TypeScript"],
+        library: ["SWR", "Next-Auth", "tailwindCSS", "Jotai"],
+        link: [{ title: "깃허브", type: "github", url: "https://github.com/ThugDev/PurrTunes" }],
+        distributeLink: "https://purr-tunes.vercel.app/",
+    },
+]
+
+export const handlers = [
+    http.get(`/api/projects`, () => {
+        return HttpResponse.json(mockProjects)
+    }),
+
+    http.get("/api/projects/:id", ({ params }) => {
+        const id = params.id
+        const project = mockProjects[Number(id)]
+        if (project) {
+            return HttpResponse.json(project)
+        } else {
+            return new HttpResponse(JSON.stringify({ message: "Project not found" }), {
+                status: 404,
+            })
+        }
+    }),
+]
