68a90411242bcdf772f6ed3d696163111eddc860
jinsol <skek298@naver.com>
2025-02-07 16:47:34 +0900

    💡refactor: header 크기 조정

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -1,13 +1,13 @@
 const Header = () => {
     return (
-        <div className="w-[100vw] h-[50px] bg-[#D4D8E5] flex flex-row justify-between items-center">
-            <div className="flex flex-row items-center ml-8">
-                <img src="/assets/image/solImage.svg" alt="sol image" className="w-[50px] h-[50px]" />
-                <p className=" font-bold text-2xl">Kim JinSol</p>
-                <p className="ml-12 text-xl">skek298@gmail.com</p>
+        <div className="w-full h-[50px] bg-[#D4D8E5] flex flex-row justify-between items-center">
+            <div className="flex flex-row items-center ml-4">
+                <img src="/assets/image/solImage.svg" alt="sol image" className="w-8 h-8" />
+                <p className=" font-bold text-lg">Kim JinSol</p>
+                <p className="ml-8 text-sm">skek298@gmail.com</p>
             </div>
             <div>
-                <p className="text-xl font-light mr-8">FrontEnd</p>
+                <p className="text-sm font-light mr-8">FrontEnd</p>
             </div>
         </div>
     )
