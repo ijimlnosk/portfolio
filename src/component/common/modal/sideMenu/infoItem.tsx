5362a4a7cb0187b26bbf9a118d8a8ab3db900685
jinsol <skek298@naver.com>
2025-02-07 22:46:03 +0900

    💡refactor: UI 위치 수정

--- src/component/common/modal/sideMenu/infoItem.tsx
+++ src/component/common/modal/sideMenu/infoItem.tsx
@@ -2,7 +2,7 @@ import { InfoItemProps } from "./type"
 
 const InfoItem = ({ icon, alt, children }: InfoItemProps) => {
     return (
-        <div className="flex flex-row gap-2 items-center my-1 cursor-pointer rounded-lg hover:bg-[#507277] hover:text-white">
+        <div className="flex flex-row gap-1 items-center my-1 cursor-pointer rounded-lg hover:bg-[#507277] hover:text-white">
             <img src={icon} alt={alt} className="w-6" />
             <div className="text-xs">{children}</div>
         </div>
