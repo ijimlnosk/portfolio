f2a2c0020411059cab0df2988d0ce3dc74834d9c
jinsol <skek298@naver.com>
2025-02-19 15:09:11 +0900

    💡refactor: 프로젝트 사이드 메뉴 아이콘 클릭 시 외부 링크로 이동 추가

--- src/component/common/modal/sideMenu/project/projectInfo.tsx
+++ src/component/common/modal/sideMenu/project/projectInfo.tsx
@@ -1,12 +1,22 @@
 import { ProjectSideItem } from "../../../../../mocks/data/type"
 
 const ProjectInfo = ({ projectInfomation }: ProjectSideItem) => {
+    const handleClick = (link: string | undefined) => {
+        if (link) {
+            window.open(link, "_blank", "noopener,noreferrer")
+        }
+    }
+
     return (
         <div className="w-full h-[300px] mt-12 flex flex-col">
             <p className="text-xl mb-1">Project Infomation</p>
             <p className="text-[8px] mb-4">아이콘을 클릭 시 관련 페이지로 이동 가능</p>
             {projectInfomation?.map((item, index) => (
-                <div key={index} className=" flex-items flex-row cursor-pointer hover:bg-[#507277] p-1 rounded-md">
+                <div
+                    key={index}
+                    onClick={() => handleClick(item.link)}
+                    className=" flex-items flex-row cursor-pointer hover:bg-[#507277] p-1 rounded-md"
+                >
                     <img src={item.image} alt="Logo" width={36} height={36} />
                     <div className="h-full ml-2 flex-justify flex-col ">
                         <div className="font-light text-[8px] -mb-1">{item.name}</div>
