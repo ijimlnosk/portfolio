4ac9d652cec8e0b20585b053bbae266c3b783211
jinsol <skek298@naver.com>
2025-03-11 19:37:40 +0900

    🟢feat: folder side 생성

--- src/component/common/modal/sideMenu/folder/folderSideMenu.tsx
+++ src/component/common/modal/sideMenu/folder/folderSideMenu.tsx
@@ -1,7 +1,13 @@
 const FolderSideMenu = () => {
     return (
-        <div className="">
-            <div>Modi</div>
+        <div className="w-full h-full relative">
+            <div className=" absolute bottom-44 left-1/4">
+                <p className="text-[#C5C5C5]">clickabel</p>
+                <div className="pt-2">
+                    <p className="font-light">📏 underline text</p>
+                    <p className="font-light">🐱 github icon</p>
+                </div>
+            </div>
         </div>
     )
 }
